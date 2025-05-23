cfbfeb8c8f9258d02cbe2182642376342fce8cbc
HappyIsland2025 <shinchan191955@gmail.com>
2025-05-23 10:09:28 +0900

    Update index.js

--- index.js
+++ index.js
@@ -118,9 +118,15 @@ ${fortuneText}`
     }
   }));
   res.status(200).end();
+});
 
 app.get('/', (req, res) => {
   res.send('LINE Bot is running.');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 
